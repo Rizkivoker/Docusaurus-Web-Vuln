86eb6bfc8a836c7d6b3845ee30d96fa25a838505
Muhammad Rizki Anshari <muh.rizki.anshari14@gmail.com>
2024-11-26 16:22:36 +0700

    Menambahkan template Docusaurus

---
+++ src/pages/my-react-page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+export default function MyReactPage() {
+    return (
+        <Layout>
+            <h1>Ini adalah halaman baru</h1>
+            <p>halaman baru</p>
+        </Layout>
+    );
+}
